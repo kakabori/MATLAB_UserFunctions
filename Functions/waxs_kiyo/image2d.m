e73421dcd24e367968802ba97f4804434e250333
Kiyo <kakabori@andrew.cmu.edu>
2014-10-09 17:44:52 -0400

    reorganized directories

--- Functions/waxs_kiyo/image2d.m
+++
@@ -1,9 +0,0 @@
-function [ output_args ] = image2d(imag,colorscale)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-colormap gray; imagesc(imag,colorscale);
-axis xy;
-
-end
-
--- Functions/waxs_kiyo/slurp_rotate.m
+++
@@ -1,9 +0,0 @@
-function a = slurp_rotate( image, angle )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-a = slurp(image,'c');
-a = imrotate(a, angle);
-
-end
-
